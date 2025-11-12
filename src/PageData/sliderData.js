a73e3a4d6e7281bffd3d864eae3b0e9dda0e49c9
Shariful Islam <67201228+shaarifulislaam@users.noreply.github.com>
2025-11-12 20:12:10 +0600

    added logo

--- src/PageData/sliderData.js
+++ src/PageData/sliderData.js
@@ -7,6 +7,7 @@ export const sliderData = [
   { id: 10, path: "/assets/images/Homepage/c5.png" },
   { id: 12, path: "/assets/images/Homepage/c6.png" },
   { id: 13, path: "/assets/images/Homepage/c7.png" },
+  { id: 133, path: "/assets/images/Homepage/c8.jpg" },
   { id: 14, path: "/assets/images/Homepage/c1.png" },
 
   { id: 17, path: "/assets/images/Homepage/c2.png" },
@@ -15,7 +16,7 @@ export const sliderData = [
   { id: 21, path: "/assets/images/Homepage/c5.png" },
   { id: 22, path: "/assets/images/Homepage/c6.png" },
 
-  { id: 23, path: "/assets/images/Homepage/c7.png" },
   { id: 24, path: "/assets/images/Homepage/c1.png" },
+  { id: 232, path: "/assets/images/Homepage/c8.jpg" },
   { id: 25, path: "/assets/images/Homepage/c2.png" },
 ];
