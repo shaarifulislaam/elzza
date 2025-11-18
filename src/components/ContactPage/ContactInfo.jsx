63a023d97e833344bdb7fc4d82e1a292a9f823c3
Shariful Islam <67201228+shaarifulislaam@users.noreply.github.com>
2025-11-18 15:31:17 +0600

    update

--- src/components/ContactPage/ContactInfo.jsx
+++ src/components/ContactPage/ContactInfo.jsx
@@ -1,4 +1,3 @@
-// components/ContactInfo.js
 import React from "react";
 
 const ContactInfo = () => {
@@ -20,7 +19,7 @@ const ContactInfo = () => {
           <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
         </svg>
       ),
-      title: "Email",
+      title: "E-mail",
       description: "Wij zullen binnen 2 werkdagen antwoorden.",
       contact: "info@elza.nl",
     },
@@ -40,7 +39,7 @@ const ContactInfo = () => {
           <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z" />
         </svg>
       ),
-      title: "Telephone",
+      title: "Telefoon",
       description:
         "Wij zijn bereikbaar van maandag tot en met vrijdag van 9.00 tot 17.00 uur.",
       contact: "+31 (22) 222 222",
@@ -62,9 +61,9 @@ const ContactInfo = () => {
           <circle cx="12" cy="10" r="3" />
         </svg>
       ),
-      title: "HQ Office",
-      description: "Visit us in our officeBezoek ons op ons kantoor",
-      contact: "Amstelstraat 54 1446 TS Amsterdam",
+      title: "Hoofdkantoor",
+      description: "Bezoek ons op ons kantoor",
+      contact: "Ampèrestraat 54 1446 TS Purmerend",
     },
   ];
 
